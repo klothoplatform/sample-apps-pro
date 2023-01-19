f674be9ad642645fb7e8930a54bf4baafc400c38
Yuval Shavit <yuval.shavit@klo.dev>
2023-01-19 18:03:35 -0500

    make py-microservices use *args / **kwargs

--- py-microservices/app/main.py
+++ py-microservices/app/main.py
@@ -24,6 +24,33 @@ async def read_item(user_id: str):
 
 
 @app.post("/users/{user_id}", response_class=PlainTextResponse)
-async def write_user(user_id: str):
-    await post_user(user_id)
+async def write_user(user_id: str, tags: str=''):
+    '''
+    Upserts a user. The tags are comma-delimited, and come in two forms, which you can mix and match:
+    
+    - "flat" tags, which are just the tag name
+    - "value" tags, which come in name=value format
+
+    For example, /users/alice?tags=foo,bar,hello=world would create three tags:
+
+    - "foo" (a flat tag)
+    - "bar" (a flat tag)
+    - "hello" => "world" (a value tag)
+    ''' 
+    flat_tags, value_tags = _parse_tags(tags)
+    await post_user(user_id, *flat_tags, **value_tags)
     return f"Created {user_id}"
+
+def _parse_tags(tags: str) -> (list[str], dict[str, str]):
+    flat_tags = []
+    value_tags = {}
+    for tag in tags.split(','):
+        if not tag:
+            continue # ignore blank tags
+        segments = tag.split('=', 1) # separate the tag by its first comma, if present
+        if len(segments) == 1:
+            flat_tags.append(tag)
+        else:
+            name, value = segments
+            value_tags[name] = value
+    return flat_tags, value_tags
